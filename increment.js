819d74647374093157213b58f258d06b67ec9c9b
David Morck <dmorck@csun.edu>
2019-04-18 14:20:48 -0700

    Refactored the click functions to work -- yiglings now buyable

--- increment.js
+++ increment.js
@@ -7,90 +7,91 @@ var foreboding = 100;
 // toggle for 2nd unit - yiglings
 var yiglingEnabled = 0;
 
- $('#buyTinyHorror').on('click', function () {
+// @todo -- refactor these click items to pass variables / switch case
+// in order to make this work for all buttons in a single function
 
-    $( "#foreboding" ).val(function (i, val) {
+$(document).on('click', '#buyTinyHorror', function () {
 
-      if (foreboding > 0) {
-        foreboding = foreboding - buyTinyHorrorCost;
+  $( "#foreboding" ).val(function (i, val) {
 
-        $( "#tinyHorrorTotal" ).val(function ( i, val ) {
-          return ++tinyHorrorTotal;
-        });
+    if (foreboding > 0) {
+      foreboding = foreboding - buyTinyHorrorCost;
 
+      $( "#tinyHorrorTotal" ).val(function ( i, val ) {
+        return ++tinyHorrorTotal;
+      });
+
+      return foreboding;
+    }
+
+    else {
+      return foreboding;
+    }
+
+  });
+
+});
+
+$(document).on('click', '#buyYigling', function () {
+
+  $( "#foreboding" ).val(function (i, val) {
+
+    if (foreboding > 0) {
+      foreboding = foreboding - buyYiglingCost;
+      $( "#yiglingTotal" ).val(function ( i, val ) {
+        return ++yiglingTotal;
+      });
         return foreboding;
       }
-
       else {
         return foreboding;
       }
 
+  });
+
+});
+
+$(document).on('click', '#sellFUD', function () {
+
+  $( "#foreboding" ).val(function (i, val) {
+
+    if (fudTotal >= 100) {
+
+      foreboding = foreboding + 10;
+
+    $( "#fudTotal").val(function (i, val) {
+      fudTotal = fudTotal - 100;
+      return fudTotal;
     });
 
+    return foreboding;
+
+    }
+
+    else {
+      return foreboding;
+    }
   });
-
-  $('#buyYigling').on('click', function () {
-
-    console.log("buyYigling Pressed");
-
-    $( "#foreboding" ).val(function (i, val) {
-      console.log("foreboding yigling fired");
-      if (foreboding > 0) {
-        foreboding = foreboding - buyYiglingCost;
-        $( "#yiglingTotal" ).val(function ( i, val ) {
-         return ++yiglingTotal;
-       });
-        return foreboding;
-      }
-      else {
-        return foreboding;
-      }
-
-    });
-  });
-
-  $('#sellFUD').on('click', function () {
-
-    $( "#foreboding" ).val(function (i, val) {
-
-     if (fudTotal >= 100) {
-
-       foreboding = foreboding + 10;
-
-       $( "#fudTotal").val(function (i, val) {
-         fudTotal = fudTotal - 100;
-         return fudTotal;
-       });
-
-       return foreboding;
-
-     }
-
-     else {
-       return foreboding;
-     }
-   });
-  });
+});
 
 $(document).ready(function(){
 
   setInterval(additionItems,1000);
 
   function additionItems () {
-    // var multiplierProduct = totalZortblorps * 2;
-    // var totalProduct = multiplierProduct + multiplierProduct;
+
     fudTotal = fudTotal + tinyHorrorTotal;
 
-    $( "#fudTotal" ).val(function (i, val) {
-      return fudTotal;
-    });
+  $( "#fudTotal" ).val(function (i, val) {
+    return fudTotal;
+  });
 
-    if (tinyHorrorTotal >= 10 && yiglingEnabled == 0) {
-      $( "#newUnits" ).after('<div class="row mb-3"><div class="col-sm-3"><label for="yiglingTotal">Yigling</label></div><div class="col-sm-5"><input type="text" value="0" id="yiglingTotal" readonly></div><div class="col-sm-4"><button type="button" id="buyYigling" class="btn btn-primary">Buy Yigling</button></div></div>');
-      yiglingEnabled = 1;
-    }
-
+  if (tinyHorrorTotal >= 10 && yiglingEnabled == 0) {
+    $( "#newUnits" ).after('<div class="row mb-3"><div class="col-sm-3"><label for="yiglingTotal">Yigling</label></div><div class="col-sm-5"><input type="text" value="0" id="yiglingTotal" readonly></div><div class="col-sm-4"><button type="button" id="buyYigling" class="btn btn-primary">Buy Yigling</button></div></div>');
+    yiglingEnabled = 1;
   }
+
+  } // function additionItems end
 
 });
 
