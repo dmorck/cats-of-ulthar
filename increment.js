7b4b4740922cd4ddc73f64ace2c0a9be6f9e835e
David Morck <dmorck@csun.edu>
2019-04-19 19:17:30 -0700

    Added chaos stones random chance

--- increment.js
+++ increment.js
@@ -6,6 +6,8 @@ var fudTotal = 0;
 var foreboding = 100;
 // toggle for 2nd unit - yiglings
 var yiglingEnabled = 0;
+// chaos stone initial value
+var chaosValue = 0;
 
 // @todo -- refactor these click items to pass variables / switch case
 // in order to make this work for all buttons in a single function
@@ -35,6 +37,8 @@ $(document).on('click', '#buyTinyHorror', function () {
 $(document).on('click', '#buyYigling', function () {
 
   $( "#foreboding" ).val(function (i, val) {
+    
+    chaosStone();
 
     if (foreboding >= 20) {
       foreboding = foreboding - buyYiglingCost;
@@ -88,18 +92,36 @@ $(document).ready(function(){
     fudTotal = fudTotal + tinyHorrorTotal + yiglingEarning;
 //    fudTotal = fudTotal + yiglingEarning;
 
-  $( "#fudTotal" ).val(function (i, val) {
-    return fudTotal;
-  });
+    $( "#fudTotal" ).val(function (i, val) {
+      return fudTotal;
+    });
 
-  if (tinyHorrorTotal >= 10 && yiglingEnabled == 0) {
-    $( "#newUnits" ).after('<div class="row mb-3"><div class="col-sm-3"><label for="yiglingTotal">Yigling</label></div><div class="col-sm-5"><input type="text" value="0" id="yiglingTotal" readonly></div><div class="col-sm-4"><button type="button" id="buyYigling" class="btn btn-primary btn-sm">Buy Yigling</button></div></div>');
-    yiglingEnabled = 1;
-  }
+    if (tinyHorrorTotal >= 10 && yiglingEnabled == 0) {
+      $( "#newUnits" ).after('<div class="row mb-3"><div class="col-sm-3"><label for="yiglingTotal">Yigling</label></div><div class="col-sm-5"><input type="text" value="0" id="yiglingTotal" readonly></div><div class="col-sm-4"><button type="button" id="buyYigling" class="btn btn-primary btn-sm">Buy Yigling</button></div></div>');
+      yiglingEnabled = 1;
+      $( ".fudInput").after('<div class="form-wrapper chaosStoneInput"><label for="chaosStones">Chaos Stones</label><input type="text" value="0" id="chaosStones" readonly></div>');
+    }
 
   } // function additionItems end
   
-});
+}); // end document ready
+
+// random function for chaos stones
+
+function chaosStone() {
+
+  var min=1;
+  var max=100;
+  var randomNumber = Math.floor(Math.random() * (+max - +min)) + +min;
+  console.log(randomNumber);
+
+  if (randomNumber >= 1 && randomNumber <= 5) {
+    chaosValue = chaosValue + 1;
+    $('#chaosStones').val(function (i, val) {
+      return chaosValue;
+    });
+  }
+}
 
 // debugging / cheat console
 // add 1000 foreboding on enter keypress (will be obfuscated after testing)
